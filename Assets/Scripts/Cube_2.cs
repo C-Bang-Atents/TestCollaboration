9f87973d6600d0962c214a25bff4e98c5b6b32ea
milinaenaru <milinae.han@gmail.com>
2023-03-01 20:51:08 +0900

    feat: Cube2 생성

---
+++ Assets/Scripts/Cube_2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_3 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Cube 생성 : 최상한");
+    }
+}
