50a281c4f9de2d76123e4e39037ddbb008a98221
JiHunIn <dryer97@naver.com>
2023-03-01 20:32:38 +0900

    feat : 큐브 5번 생성

---
+++ Assets/Scripts/Cube_5.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_5 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Cube 생성자 : 인지훈");
+    }
+}
